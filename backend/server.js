6ae4e9c3a9d6c59e3252f07b0ae0a6db60b32e4a
Khoa <s3836606@rmit.edu.vn>
2024-08-12 14:04:44 +0700

    create project

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
+
+// Run server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
